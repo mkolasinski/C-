07f66353e6898271d734349f36002266d3fff11e
mkolasinski <m.kolasinski@poczta.fm>
2018-06-19 22:23:14 +0200

    Current Date

---
+++ Current Date/Current Date/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Current_Date
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            Console.Write("Now is: " + today);
+            Console.ReadKey();
+        }
+    }
+}
