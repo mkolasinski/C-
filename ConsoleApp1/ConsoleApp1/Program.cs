2251345a1ec4847f10473cbe7899f144e1b727ce
mkolasinski <m.kolasinski@poczta.fm>
2018-06-19 19:02:12 +0200

    Suma liczb nieparzystych

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int result = 0;
+        int[] tab = new int[] { 1, 2, 3, 4, 7, 8, 9, 11, 14, 15, 16, 17, 19, 22 };
+
+        for (int i = 0; i < tab.Length; i++)
+        {
+            if (i % 2 == 1)
+            {
+                result = result + i;
+            }
+        }
+        Console.Write("Suma liczb nieparzystych: " + result);
+        Console.ReadLine();
+    }
+}
