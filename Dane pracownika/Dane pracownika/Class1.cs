8b05139fc39365eda92735cc435e6da23a0f8410
mkolasinski <m.kolasinski@poczta.fm>
2018-06-24 11:44:14 +0200

    Add new small programm to display employer data

---
+++ Dane pracownika/Dane pracownika/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dane_pracownika
+{
+    public class Pracownik
+    {
+        public string Imie { get; set; }
+        public int Wiek { get; set; }
+        public int Pensja { get; set; }
+    }
+}
---
+++ Dane pracownika/Dane pracownika/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dane_pracownika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pracownik osoba = new Pracownik();
+            osoba.Imie = "Michał";
+            osoba.Wiek = 27;
+            osoba.Pensja = 4000;
+            Console.WriteLine("Imię pracownika: " + osoba.Imie);
+            Console.WriteLine("Wiek pracownika: " + osoba.Wiek);
+            Console.WriteLine("Pensja: " + osoba.Pensja);
+            Console.WriteLine("--------------");
+            Console.ReadLine();
+        }
+    }
+}
