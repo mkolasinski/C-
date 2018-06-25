69cc3aaef948d93aa9de47f22ae10b0d9fc4b748
mkolasinski <m.kolasinski@poczta.fm>
2018-06-25 22:41:58 +0200

    Average of four numbers

---
+++ Average of numbers/Average of numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Average_of_numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            double n1, n2, n3, n4, result;
+
+            Console.Write("Enter First Number: ");
+            n1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter Second Number: ");
+            n2 = double.Parse(Console.ReadLine());
+            Console.Write("Enter Third Number: ");
+            n3 = double.Parse(Console.ReadLine());
+            Console.Write("Enter Fourth Number: ");
+            n4 = double.Parse(Console.ReadLine());
+
+            result = (n1 + n2 + n3 + n4)/4;
+            Console.ReadLine();
+            Console.WriteLine("The average is: " + result);
+            Console.ReadLine();
+        }
+    }
+}
