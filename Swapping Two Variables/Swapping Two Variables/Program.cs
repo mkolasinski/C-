102c5d680be5a26c9d85594341ac624fc0a3d740
mkolasinski <m.kolasinski@poczta.fm>
2018-06-25 23:02:17 +0200

    Swapping Two Variables

---
+++ Swapping Two Variables/Swapping Two Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Swapping_Two_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the First Number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Input the Second Number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int swap = a;
+            a = b;
+            b = swap;
+            Console.WriteLine("------");
+            Console.WriteLine("After Swapping is: ");
+            Console.WriteLine("Number one is: " + a);
+            Console.WriteLine("Number two is: " + b);
+            Console.ReadLine();
+        }
+    }
+}
