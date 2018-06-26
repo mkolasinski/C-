b54c6d2a08f07f7853b298fc8609de5cb49796fc
mkolasinski <m.kolasinski@poczta.fm>
2018-06-26 21:18:22 +0200

    Sum of elements in array

---
+++ Sum of elements in array/Sum of elements in array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sum_of_elements_in_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 5, 8, 9, 12, 19, 24, 33 };
+            Console.WriteLine("Array: [{0}]", string.Join(",", array));
+            Console.ReadLine();
+
+            int sum = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            Console.WriteLine("Sum of elements: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
