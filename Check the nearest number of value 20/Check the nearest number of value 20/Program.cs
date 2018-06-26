88dc13a0d684001e0ab0268455b3fc9c438cd0f1
mkolasinski <m.kolasinski@poczta.fm>
2018-06-26 18:36:02 +0200

    Check the nearest number

---
+++ Check the nearest number of value 20/Check the nearest number of value 20/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Check_the_nearest_number_of_value_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int value = 20;
+
+            int value1 = Math.Abs(a - value);
+            int value2 = Math.Abs(b - value);
+            Console.WriteLine(
+                value1 == value2 ? 0 : ( value1 < value2 ? a : b));
+            Console.ReadLine();
+        }
+    }
+}
