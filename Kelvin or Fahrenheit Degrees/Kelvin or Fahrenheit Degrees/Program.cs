7f1ffed40719fd9f41add8908254ae74577ebc3b
mkolasinski <m.kolasinski@poczta.fm>
2018-06-26 17:41:02 +0200

    Kelvin and Fahrenheit Degrees

---
+++ Kelvin or Fahrenheit Degrees/Kelvin or Fahrenheit Degrees/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Kelvin_or_Fahrenheit_Degrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Celsius Degrees: ");
+            int degrees = int.Parse(Console.ReadLine());
+            int kelvin = degrees + 273;
+            int fahrenheit = degrees * 18 / 10 + 32;
+
+            Console.WriteLine("Kelvin's Degrees = " + kelvin);
+            Console.WriteLine("Fahrenheit's Degrees = " + fahrenheit);
+            Console.ReadLine();
+        }
+    }
+}
