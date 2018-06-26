e926038ad3ca374f49bd025b3285e83b40febf5a
mkolasinski <m.kolasinski@poczta.fm>
2018-06-26 17:46:45 +0200

    Number in Rectangle

---
+++ Number in Rectangle/Number in Rectangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Number_in_Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.ReadLine();
+            Console.WriteLine("{0}{0}{0}", a);
+            Console.WriteLine("{0} {0}", a);
+            Console.WriteLine("{0} {0}", a);
+            Console.WriteLine("{0} {0}", a);
+            Console.WriteLine("{0}{0}{0}", a);
+            Console.ReadLine();
+        }
+    }
+}
