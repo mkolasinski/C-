0ed2e8c43425f61849f760fcd98328b1b2a9b664
mkolasinski <m.kolasinski@poczta.fm>
2018-06-26 17:53:34 +0200

    User's Age

---
+++ User Age/User Age/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace User_Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("You Look Younger than {0} ", age);
+            Console.ReadLine();
+        }
+    }
+}
