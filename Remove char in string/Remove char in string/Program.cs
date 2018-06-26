a08e4b1b66d41e3b8b6ce3fe43d9ae0d09ec8a03
mkolasinski <m.kolasinski@poczta.fm>
2018-06-26 18:01:37 +0200

    Remove Char in String

---
+++ Remove char in string/Remove char in string/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Remove_char_in_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(remove_char("MicrosoftVisualStudio", 4));
+            Console.WriteLine(remove_char("MicrosoftVisualStudio", 11));
+            Console.WriteLine(remove_char("MicrosoftVisualStudio", 18));
+            Console.ReadLine();
+        }
+        public static string remove_char(string str, int i)
+        {
+            return str.Remove(i, 1);
+        }
+    }
+}
