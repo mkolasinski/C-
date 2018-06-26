9ba760b800daedfe3968b1eca26c37a3bd27e684
mkolasinski <m.kolasinski@poczta.fm>
2018-06-26 18:13:39 +0200

    Sum Of Two Integers

---
+++ Sum of two integers/Sum of two integers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sum_of_two_integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOf(2, 5));
+            Console.WriteLine(SumOf(7, 7));
+            Console.WriteLine(SumOf(2, 13));
+            Console.ReadLine();
+        }
+        public static int SumOf(int x, int y)
+        {
+            return x == y ? (x + y) * 3 : x + y;
+        }
+    }
+}
