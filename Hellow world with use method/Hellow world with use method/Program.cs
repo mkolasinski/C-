9e3b1eee8b553c824bdc63bf801b30443db6e95e
mkolasinski <m.kolasinski@poczta.fm>
2018-06-26 19:41:40 +0200

    Hello with use methods

---
+++ Hellow world with use method/Hellow world with use method/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Hellow_world_with_use_method
+{
+    class Program
+    {
+        public static void hello()
+        {
+            Console.WriteLine("Welcome!");
+            Console.ReadLine();
+        }
+
+        public static void HaveNice()
+        {
+            Console.WriteLine("Have a Nice Day!");
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            hello();
+            HaveNice();
+        }
+    }
+}
