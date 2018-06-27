fd2139321287f334c2d7a1b5a62b1f9bc097a2fe
mkolasinski <m.kolasinski@poczta.fm>
2018-06-27 17:47:41 +0200

    To Calculate the sum of individual number

---
+++ Calculate the sum of individual number/Calculate the sum of individual number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculate_the_sum_of_individual_number
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter an individual number to calculate its sum: ");
+            int x = int.Parse(Console.ReadLine());
+
+            string y = Convert.ToString(x);
+            int sum = 0;
+            for (int i = 0; i < y.Length; i++)
+            {
+                sum += Convert.ToInt32(y.Substring(i, 1));
+            }
+
+            Console.WriteLine("The sum is equal: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
