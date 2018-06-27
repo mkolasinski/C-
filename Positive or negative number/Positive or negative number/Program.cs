ef36c82354e51af47f34dc70f921c09661fe564a
mkolasinski <m.kolasinski@poczta.fm>
2018-06-27 17:59:35 +0200

    Positive or negative number

---
+++ Positive or negative number/Positive or negative number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Positive_or_negative_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Compare two integers!");
+            Console.WriteLine("Enter your number:");
+            num = int.Parse(Console.ReadLine());
+
+            if ( num < 0 )
+            {
+                Console.WriteLine("Your number is negative!");
+                Console.ReadKey();
+            } else if ( num > 0)
+            {
+                Console.WriteLine("Your number is positive :)");
+                Console.ReadKey();
+            }
+            else if ( num == 0 )
+            {
+                Console.WriteLine("Your number is 0.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
