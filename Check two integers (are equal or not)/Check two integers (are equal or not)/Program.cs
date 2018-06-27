a0f1fba0733f43bcc3193d12d18f76d83bc17f7f
mkolasinski <m.kolasinski@poczta.fm>
2018-06-27 17:53:30 +0200

    Check two integers (equal or not)

---
+++ Check two integers (are equal or not)/Check two integers (are equal or not)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Check_two_integers__are_equal_or_not_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program can to compare You two integers.");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("Enter first number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.ReadLine();
+
+            if ( a == b )
+            {
+                Console.WriteLine("These numbers: {0} and {1} are equal", a, b);
+                Console.ReadLine();
+            } else
+            {
+                Console.WriteLine("These numbers are not equal");
+                Console.ReadKey();
+            }
+        }
+    }
+}
