c09c7d30ffc538427d75a68b9760a4867258fc49
mkolasinski <m.kolasinski@poczta.fm>
2018-06-27 18:12:37 +0200

    Leap year or not

---
+++ Leap year or not/Leap year or not/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Leap_year_or_not
+{
+    class Program
+    {
+        static void Main()
+        {
+            int year;
+            Console.WriteLine("Enter an year");
+            Console.WriteLine("-------------------------------");
+
+            Console.Read();
+            year = int.Parse(Console.ReadLine());
+
+            if ( (year % 4) == 0 ) {
+                Console.WriteLine("The year" + year + " is a leap");
+            } else if ( (year % 400) == 0 )
+            {
+                Console.WriteLine("The year" + year + " is a leap");
+            } else if ( (year % 100) == 0 )
+            {
+                Console.WriteLine("The year" + year + " is not a leap");
+            } else
+            {
+                Console.WriteLine("The year" + year + " is a not a leap");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
