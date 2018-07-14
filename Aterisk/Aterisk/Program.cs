b5544e22bd5a4f18424fe03f2ce8cecec847fb8c
mkolasinski <m.kolasinski@poczta.fm>
2018-07-14 22:20:19 +0200

    Display the pattern like right angle using asterisk

---
+++ Aterisk/Aterisk/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aterisk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows;
+            Console.Write("\n\n");
+            Console.Write("Display the pattern like right angle using asterisk:\n");
+            Console.Write("------------------------------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input number of rows : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aterisk/Aterisk/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Aterisk")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Aterisk")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("2dafcea4-c2a5-42d9-925b-1fef3fcc7437")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
