03a9cf4199beae9af6bfbb1240446d6de021661b
mkolasinski <m.kolasinski@poczta.fm>
2018-07-14 22:01:56 +0200

    The cube of the number

---
+++ Find cube of the number/Find cube of the number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Find_cube_of_the_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, i;
+            Console.WriteLine("Input number");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            for ( i = 1; i <= a; i++)
+            {
+                Console.WriteLine("Number is: {0} and cube of the {1} is: {2} \n", i,i, (i * i * i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
