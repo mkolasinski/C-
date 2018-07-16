aa688d4b91dd510dd66145b074df34c44cd505b7
mkolasinski <m.kolasinski@poczta.fm>
2018-07-16 20:29:49 +0200

    To calculate the result of raising an integer number to another

---
+++ Result of raising an integer number to another/Result of raising an integer number to another/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Result_of_raising_an_integer_number_to_another
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, total;
+            Console.WriteLine("Calculate the result of raising an integer number to another: ");
+            Console.WriteLine("-------------------------------");
+
+            Console.WriteLine("Input a Base: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input an Exponent: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            total = Math.Pow(a, b);
+            Console.WriteLine("Total: {0} ", total);
+            Console.ReadKey();
+        }
+    }
+}
