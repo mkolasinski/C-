2c740d69fd6876f2dc966605996395dc362381aa
mkolasinski <m.kolasinski@poczta.fm>
2018-07-16 19:25:58 +0200

    Print the odd numbers from 1 to 99

---
+++ Print the odd numbers from 1 to 99/Print the odd numbers from 1 to 99/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_the_odd_numbers_from_1_to_99
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print the odd numbers from 1 to 99");
+            Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+            for ( int i = 1; i <= 99; i++ )
+            {
+                if ( i % 2 != 0 )
+                {
+                    Console.WriteLine(i);
+                } 
+            }
+            Console.ReadKey();
+        }
+    }
+}
