d393ba92d9b1874d4d583f0ede2456bd18a37c41
mkolasinski <m.kolasinski@poczta.fm>
2018-07-16 20:53:02 +0200

    Separate the individual characters from a string

---
+++ Separate the individual characters/Separate the individual characters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Separate_the_individual_characters
+{
+    class Program
+    {
+        static void Main()
+        {
+            string word;
+            int length = 0;
+
+            Console.WriteLine("Separate an individual characters from a string");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("Input the word: ");
+
+            word = Console.ReadLine();
+
+            Console.WriteLine("");
+            Console.WriteLine("The characters of the string are: ");
+            Console.WriteLine("");
+
+            while ( length <= word.Length - 1)
+            {
+                Console.WriteLine("{0} ", word[length]);
+                length++;
+            }
+
+            Console.Write("\n\n");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Separate the individual characters/Separate the individual characters/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Separate the individual characters")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Separate the individual characters")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("127a6aaa-ab40-4bff-b3b2-9d91e06087a6")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
