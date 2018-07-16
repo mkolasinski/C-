292484e3397917bf9681feea2edd9c7dc56d320e
mkolasinski <m.kolasinski@poczta.fm>
2018-07-16 19:34:18 +0200

    Check if a given positive number is a multiple of 3 or a multiple of 7

---
+++ Positive number-multiple of 3 or 7/Positive number-multiple of 3 or 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Positive_number_multiple_of_3_or_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input first integer: ");
+            int a = int.Parse(Console.ReadLine());
+
+            if ( a > 0 )
+            {
+                Console.WriteLine(a % 3 == 0 || a % 7 == 0);
+            }
+            Console.ReadKey();
+        }
+    }
+}
