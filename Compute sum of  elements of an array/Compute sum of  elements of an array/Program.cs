003e25edf89dd0c1817e9836d291bc1150cb82d1
mkolasinski <m.kolasinski@poczta.fm>
2018-07-16 19:41:59 +0200

    Compute sum of all the elements of an array of integers

---
+++ Compute sum of  elements of an array/Compute sum of  elements of an array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compute_sum_of__elements_of_an_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 10, 1, 2, 3, 4, 5, 6, 8, 9, 0 };
+            Console.WriteLine("Array: [{0}]", string.Join(", ", numbers));
+
+            int total = 0;
+
+            for ( int i = 0; i < numbers.Length; i++ )
+            {
+                total += numbers[i];
+            }
+            Console.WriteLine("Sum: " + total);
+            Console.ReadKey();
+        }
+    }
+}
