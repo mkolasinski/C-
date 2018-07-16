23c905e305fe110302da4fc085229a563ffde4a8
mkolasinski <m.kolasinski@poczta.fm>
2018-07-16 21:38:11 +0200

    Display the pattern like a pyramid with a number which will repeat the number in the same row

---
+++ Pyramid with numbers/Pyramid with numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Pyramid_with_numbers
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int row, spc, k, i, j;
+            Console.WriteLine("Display the pattern like pyramid with repeating a numbers with in same row.");
+            Console.WriteLine("");
+
+            Console.WriteLine("Input a number of rows: ");
+            row = int.Parse(Console.ReadLine());
+
+            spc = row + 4 - 1;
+
+            for ( i = 1; i <= row; i++)
+            {
+                for ( k = spc; k >= 1; k--)
+                {
+                    Console.Write(" ");
+                }
+
+                for ( j = 1; j <= i; j++)
+                {
+                    Console.Write("{0} ", i);
+                }
+                Console.Write("\n");
+                spc--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
