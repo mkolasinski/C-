64d07d1ca30dbcebd3a6a33cf01fb9b8ba49c01d
mkolasinski <m.kolasinski@poczta.fm>
2018-08-04 21:37:26 +0200

    Crate an empty file on the disk

---
+++ Create a file/Create a file/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System;
+using System.Text;
+
+namespace Create_a_file
+{
+    class Program
+    {
+        static void Main()
+        {
+            string fileName = @"mytext.txt";
+            try
+            {
+                Console.WriteLine("Create a file on your disk.");
+                Console.WriteLine("------------------------------------");
+
+                //Create a file.
+
+                using (FileStream fileStr = File.Create(fileName));
+                {
+                    Console.WriteLine("A file was created.");
+                }
+            }
+            catch (Exception MyExcep)
+            {
+                Console.WriteLine("MyExcep.ToString()");
+            }
+        }
+    }
+}
