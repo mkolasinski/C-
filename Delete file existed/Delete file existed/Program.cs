67ffe95e9e4e8a9c9c2fdb13fe6f0ff5e5490ff2
mkolasinski <m.kolasinski@poczta.fm>
2018-08-04 22:00:53 +0200

    Remove the file

---
+++ Delete file existed/Delete file existed/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Delete_file_existed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"mytext.txt";
+            Console.WriteLine("Remove file which exists");
+            Console.WriteLine("--------------------------------");
+
+            //Delete
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+                Console.WriteLine("File {0} was deleted successfully!", fileName);
+            } else
+            {
+                Console.WriteLine("File {0} does not exist.", fileName);
+                Console.ReadKey();
+            }
+        }
+    }
+}
