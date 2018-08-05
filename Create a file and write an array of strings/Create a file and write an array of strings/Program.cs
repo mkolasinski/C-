5c34f52337ea36b64948321943a4a816ba2d7fbd
mkolasinski <m.kolasinski@poczta.fm>
2018-08-05 11:54:23 +0200

    Create a file and write an array of strings

---
+++ Create a file and write an array of strings/Create a file and write an array of strings/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Create_a_file_and_write_an_array_of_strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string fileName = @"mytest.txt";
+            string[] Array;
+
+            Console.WriteLine("Create a file and write an array of strings");
+            Console.WriteLine("----------------------------------------------");
+
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine(" Input a number of lines to write in file: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            Array = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Line {0}:", i + 1);
+                Array[i] = Console.ReadLine();
+            }
+
+            System.IO.File.WriteAllLines(fileName, Array);
+
+            using (StreamReader str = File.OpenText(fileName))
+            {
+                string s = " ";
+                Console.WriteLine("The content of Your file is: ");
+                Console.WriteLine("");
+
+                while (( s = str.ReadLine() ) != null )
+                {
+                    Console.WriteLine(" {0} ", s);
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Create a file and write an array of strings/Create a file and write an array of strings/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Create a file and write an array of strings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Create a file and write an array of strings")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("c58d659b-8d93-4118-bd28-889528414817")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
