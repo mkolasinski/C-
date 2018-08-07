16a36a04496a975ad548f2375bff0fcd6168fdd3
mkolasinski <m.kolasinski@poczta.fm>
2018-08-07 20:35:08 +0200

    Sort numbers in array

---
+++ Sort numbers in array/Sort numbers in array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_numbers_in_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7] { 1, 14, 22, 89, 90, 2, 3 };
+            int temp = 0, i, j;
+
+            for ( i = 0; i < array.Length; i++)
+            {
+                for ( j = 0; j < array.Length; j++)
+                {
+                    if ( array[i] < array[j] )
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted numbers: ");
+            for ( i = 0; i < array.Length; i++ )
+            {
+                Console.WriteLine("{0}", array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
