d549bc0a260947fc71980a0902d75d473667b497
mkolasinski <m.kolasinski@poczta.fm>
2018-08-07 21:10:46 +0200

    Palindrom 3

---
+++ Palindrom 3/Palindrom 3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom_3
+{
+    class Program
+    {
+        public static void Reverse()
+        {
+            string inputStr, reverseStr;
+
+            Console.WriteLine("Input your string:");
+            inputStr = Console.ReadLine();
+            reverseStr = new string(inputStr.Reverse().ToArray());
+
+            if (inputStr == reverseStr)
+            {
+                Console.WriteLine("Your string is a palindrom.");
+            }
+            else 
+            {
+                Console.WriteLine("Try Again. It is not a palindrom.");
+                Console.WriteLine("");
+                Reverse();
+            }
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Check if your string is a palindrom");
+            Console.WriteLine("--------------------------------------");
+
+            Reverse();
+
+            Console.ReadKey();
+        }
+    }
+}
