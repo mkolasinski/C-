fe778dfc35e9c096de532ee2e6319cfd1142e1c2
mkolasinski <m.kolasinski@poczta.fm>
2018-08-07 20:14:26 +0200

    Palindrom 1

---
+++ Palindrom - check if string is a palindrom/Palindrom - check if string is a palindrom/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom___check_if_string_is_a_palindrom
+{
+    class Program
+    {
+        static void Main()
+        {
+            string inputString = "one two three";
+            string resultString = string.Join(" ", inputString.Split(' ').Select(x => new String(x.Reverse().ToArray())));
+            Console.WriteLine(resultString);
+            Console.ReadKey();
+        }
+    }
+}
