bf6460dbe1a9c7db645eacda10314996c1d52480
mkolasinski <m.kolasinski@poczta.fm>
2018-08-07 20:25:41 +0200

    Palindrom 2

---
+++ Palindrom 2/Palindrom 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString, reverseString;
+
+            Console.WriteLine("Check if your word is a palindrom");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("");
+
+            Console.WriteLine("Input your word:");
+            inputString = Console.ReadLine();
+            reverseString = new string(inputString.Reverse().ToArray());
+            
+            if ( inputString == reverseString )
+            {
+                Console.WriteLine("Your word is a PALINDROM!");
+                Console.WriteLine(reverseString);
+            } else
+            {
+                Console.WriteLine("Try Again! Your word is not a palindrom...");
+            }
+            Console.ReadKey();
+        }
+    }
+}
