e2329ba1398222e7720fe553f634677e8a5c4924
mkolasinski <m.kolasinski@poczta.fm>
2018-08-29 19:14:35 +0200

    Add given number of months

---
+++ Add given number of months/Add given number of months/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Add_given_number_of_months
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = new DateTime(2018, 8, 29);
+            int x;
+            int y = 100;
+
+            for (x = 0; x <= y; x++)
+            {
+                Console.WriteLine(date.AddMonths(x).ToString("d"));
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
