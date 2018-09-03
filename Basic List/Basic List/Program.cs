04b8efbcec867606a0fe2c0bec623c9c7dcaf26b
mkolasinski <m.kolasinski@poczta.fm>
2018-09-03 22:06:46 +0200

    Dodanie maksymalnego wieku i minimalnego

--- Basic List/Basic List/Program.cs
+++ Basic List/Basic List/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Person
 {
@@ -19,10 +20,10 @@ public class Example
 {
     public static void Main()
     {
-        List <Person> people = new List<Person>();
+        List<Person> people = new List<Person>();
 
-        people.Add(new Person() { Id=1, Name = "John", Age = 24 });
-        people.Add(new Person() { Id=2, Name = "Tom", Age = 27 });
+        people.Add(new Person() { Id=1, Name = "John", Age = 24, });
+        people.Add(new Person() { Id=2, Name = "Tom", Age = 31 });
         people.Add(new Person() { Id = 3, Name = "Simon", Age = 34 });
 
         foreach (Person element in people)
@@ -32,6 +33,12 @@ public class Example
             Console.WriteLine("Age: " + element.Age);
             Console.WriteLine("---------------");
         }
+
+        int maxAge = people.Max(s => s.Age);
+        int minAge = people.Min(s => s.Age);
+
+        Console.WriteLine("The oldest people is: {0}", maxAge);
+        Console.WriteLine("The youngest people is: {0}", minAge);
 
         Console.ReadKey();
     }
