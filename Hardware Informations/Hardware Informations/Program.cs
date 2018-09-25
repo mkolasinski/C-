9e927e8a38b2d436c3df0b5a6a43fcf27a0ec6b1
mkolasinski <m.kolasinski@poczta.fm>
2018-09-25 20:27:42 +0200

    Basic Hardware Informations. Very Very simple things

---
+++ Hardware Informations/Hardware Informations/Program.cs
@@ -0,0 +1,49 @@
+﻿#region Usings
+
+using System;
+using System.Management;
+using System.Management.Instrumentation;
+
+#endregion
+
+namespace Hardware_Informations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            General();
+            OsVersion();
+            PcName();
+            WindowsFolder();
+            LogicalDrives();
+            Console.ReadKey();
+        }
+
+        public static void General()
+        {
+            Console.WriteLine("Display general informations about your system.........");
+            Console.WriteLine("");
+        }
+
+        public static void OsVersion()
+        {
+            Console.WriteLine("Windows Version: {0}", Environment.OSVersion.ToString());
+        }
+
+        public static void PcName()
+        {
+            Console.WriteLine("PcName is: {0}", Environment.MachineName);
+        }
+
+        public static void WindowsFolder()
+        {
+            Console.WriteLine("Windows folder is: {0}", Environment.SystemDirectory);
+        }
+
+        public static void LogicalDrives()
+        {
+            Console.WriteLine("Logical Drives: {0}", Environment.GetLogicalDrives());
+        }
+    }
+}
