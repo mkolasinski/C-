5fb4850e1b80fd9e415e3e0f0a90b9396dac4ce8
mkolasinski <m.kolasinski@poczta.fm>
2018-10-08 11:58:17 +0200

    Get CPU Details

---
+++ Get computer hardware information/Get computer hardware information/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetCpuDetails();
+        }
+        private static void GetCpuDetails()
+        {
+            foreach (var item in new System.Management.ManagementObjectSearcher("Select * from Win32_ComputerSystem").Get())
+            {
+                Console.WriteLine("Number Of Physical Processors: {0} ", item["NumberOfProcessors"]);
+                Console.WriteLine("Number Of Logical Processors: {0} ", item["NumberOfLogicalProcessors"]);
+            }
+
+            var numberOfCores = 0;
+            foreach (var item in new System.Management.ManagementObjectSearcher("Select * from Win32_Processor").Get())
+            {
+                numberOfCores += int.Parse(item["NumberOfCores"].ToString());
+                Console.WriteLine("Bitness: {0}", item["AddressWidth"]);
+                Console.WriteLine("Architecture: {0}", GetArchitectureDetail(int.Parse(item["Architecture"].ToString())));
+            }
+
+            Console.WriteLine("Number Of Cores: {0}", numberOfCores);
+
+
+        }
+
+        private static string GetArchitectureDetail(int architectureNumber)
+        {
+            switch (architectureNumber)
+            {
+                case 0: return "x86";
+                case 1: return "MIPS";
+                case 2: return "Alpha";
+                case 3: return "PowerPC";
+                case 6: return "Itanium-based systems";
+                case 9: return "x64";
+                default:
+                    return "Unkown";
+            }
+        }
+    }
+}
---
+++ Get computer hardware information/Get computer hardware information/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Get computer hardware information")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Get computer hardware information")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("32c64a10-7089-40d6-99a4-398d3e7bf20e")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
